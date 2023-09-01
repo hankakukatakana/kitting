c188a7ecd608bdc5688008172c48c337daf00437
はらだ <65936854+hankakukatakana@users.noreply.github.com>
2023-09-01 18:01:50 +0900

    aaaa

--- kitting.ps1
+++ kitting.ps1
@@ -16,33 +16,38 @@ function Txt_Clear(){
     New-Item .\Result.txt -ItemType file -Force
 }
 function GetJson(){
-    $JsonDate = (Get-Content ".\installurl.json")
+    $JsonDate = (Get-Content ".\install.json")
 }
 
 $Form = New-Object System.Windows.Forms.Form
 	$Form.Size = New-Object System.Drawing.Size(500,500)
 	$Form.Text = "TimeStamp"
-$Ckbox = New-Object System.Windows.Forms.CheckBox
-    $Ckbox.Size = New-Object System.Drawing.Size(50,20)
-    $Ckbox.Text = "test"
+
+
+$Ckbox = New-Object System.Windows.Forms.CheckedListBox
+$Ckbox.Dock = [System.Windows.Forms.DockStyle]::Fill
+$array = @("test1","test2","test3","test4")
+foreach($item in $array){
+    $Ckbox.Items.Add($item)
+}
 $Form.Controls.Add($Ckbox)
 
 $DateBtn = New-Object System.Windows.Forms.Button
-	$DateBtn.Location = New-Object System.Drawing.Point(0,30)
+	$DateBtn.Location = New-Object System.Drawing.Point(0,300)
 	$DateBtn.Size = New-Object System.Drawing.Size(100,30)
 	$DateBtn.Text = "Install"
 $Form.Controls.Add($DateBtn)
 $DateBtn.Add_Click({Time_Stamp})
 
 $TxtBtn = New-Object System.Windows.Forms.Button
-	$TxtBtn.Location = New-Object System.Drawing.Point(100,30)
+	$TxtBtn.Location = New-Object System.Drawing.Point(100,300)
 	$TxtBtn.Size = New-Object System.Drawing.Size(100,30)
 	$TxtBtn.Text = "All Check"
 $Form.Controls.Add($TxtBtn)
 $TxtBtn.Add_Click({Txt_Open})
 
 $ClearBtn = New-Object System.Windows.Forms.Button
-	$ClearBtn.Location = New-Object System.Drawing.Point(150,30)
+	$ClearBtn.Location = New-Object System.Drawing.Point(200,300)
 	$ClearBtn.Size = New-Object System.Drawing.Size(100,30)
 	$ClearBtn.Text = "All Del"
 $Form.Controls.Add($ClearBtn)
