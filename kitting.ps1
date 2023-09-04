b402aeefd891b8fce6bbbc45bb8ec54ba73a9b64
はらだ <65936854+hankakukatakana@users.noreply.github.com>
2023-09-04 12:30:12 +0900

    Update kitting.ps1

--- kitting.ps1
+++ kitting.ps1
@@ -15,19 +15,15 @@ function Get-UrlsFromJson {
 }
 
 # JSONファイルからURLを取得
-$urls = Get-UrlsFromJson -JsonFilePath "C:\Path\To\urls.json"
+$urls = Get-UrlsFromJson -JsonFilePath ""I:\kitting\App\install.json""
 
 # 取得したURLを表示
 $urls
 
-function GetJson(){
-    $JsonDate = (Get-Content ".\install.json")
-}
-
+#-----UI-------
 $Form = New-Object System.Windows.Forms.Form
 	$Form.Size = New-Object System.Drawing.Size(500,500)
-	$Form.Text = "TimeStamp"
-
+	$Form.Text = "Kitting tool"
 
 $Ckbox = New-Object System.Windows.Forms.CheckedListBox
 $Ckbox.Dock = [System.Windows.Forms.DockStyle]::Fill
@@ -42,21 +38,21 @@ $DateBtn = New-Object System.Windows.Forms.Button
 	$DateBtn.Size = New-Object System.Drawing.Size(100,30)
 	$DateBtn.Text = "Install"
 $Form.Controls.Add($DateBtn)
-$DateBtn.Add_Click({Time_Stamp})
+$DateBtn.Add_Click({})
 
 $TxtBtn = New-Object System.Windows.Forms.Button
 	$TxtBtn.Location = New-Object System.Drawing.Point(100,300)
 	$TxtBtn.Size = New-Object System.Drawing.Size(100,30)
 	$TxtBtn.Text = "All Check"
 $Form.Controls.Add($TxtBtn)
-$TxtBtn.Add_Click({Txt_Open})
+$TxtBtn.Add_Click({})
 
 $ClearBtn = New-Object System.Windows.Forms.Button
 	$ClearBtn.Location = New-Object System.Drawing.Point(200,300)
 	$ClearBtn.Size = New-Object System.Drawing.Size(100,30)
 	$ClearBtn.Text = "All Del"
 $Form.Controls.Add($ClearBtn)
-$ClearBtn.Add_Click({Txt_Clear})
+$ClearBtn.Add_Click({})
 
 $Form.Add_Shown({$Form.Activate()})
 $Form.ShowDialog()
