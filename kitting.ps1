e6b06b44e767d28833712b942afc78aeb7fe0159
はらだ <65936854+hankakukatakana@users.noreply.github.com>
2023-09-04 15:10:36 +0900

    komi

--- kitting.ps1
+++ kitting.ps1
@@ -1,8 +1,8 @@
-﻿
-Add-Type -AssemblyName System.Drawing
+﻿Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName System.Windows.Forms
 
-
+# JSONファイルからURLを取得
+'
 function Get-UrlsFromJson {
     param (
         [string]$JsonFilePath
@@ -13,46 +13,57 @@ function Get-UrlsFromJson {
     $urls = $data | ForEach-Object { $_.url }
     return $urls
 }
-
-# JSONファイルからURLを取得
-$urls = Get-UrlsFromJson -JsonFilePath ""I:\kitting\App\install.json""
-
-# 取得したURLを表示
-$urls
+$urls = Get-UrlsFromJson -JsonFilePath "I:\kitting\App\testurl.json"
+'
+$urls = @(	
+    "https://support.zoom.us/hc/ja/articles/207373866-Zoom-%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%A9",
+	"https://www.microsoft.com/ja-jp/microsoft-teams/download-app",
+	"https://slack.com/intl/ja-jp/downloads/windows",
+	"https://www.google.com/intl/ja/chrome/",
+	"https://support.mozilla.org/ja/kb/how-download-and-install-firefox-windows?redirect=no",
+	"https://vm-islo-m1.radix.ad.jp/users/main/downloads.html",
+	"https://forest.watch.impress.co.jp/library/software/ipmsg/"
+)
+function OpenURL{
+    foreach($url in $urls){
+        Start-Process $url
+    } 
+}
 
 #-----UI-------
 $Form = New-Object System.Windows.Forms.Form
 	$Form.Size = New-Object System.Drawing.Size(500,500)
 	$Form.Text = "Kitting tool"
-
+'
 $Ckbox = New-Object System.Windows.Forms.CheckedListBox
 $Ckbox.Dock = [System.Windows.Forms.DockStyle]::Fill
-$array = @("test1","test2","test3","test4")
-foreach($item in $array){
+
+foreach($item in $urls){
     $Ckbox.Items.Add($item)
+    $Ckbox.Size = New-Object System.Drawing.Size(200,30)
 }
 $Form.Controls.Add($Ckbox)
+'
+$Install = New-Object System.Windows.Forms.Button
+	$Install.Location = New-Object System.Drawing.Point(0,300)
+	$Install.Size = New-Object System.Drawing.Size(100,30)
+	$Install.Text = "Install"
+$Form.Controls.Add($Install)
+$Install.Add_Click({OpenURL})
 
-$DateBtn = New-Object System.Windows.Forms.Button
-	$DateBtn.Location = New-Object System.Drawing.Point(0,300)
-	$DateBtn.Size = New-Object System.Drawing.Size(100,30)
-	$DateBtn.Text = "Install"
-$Form.Controls.Add($DateBtn)
-$DateBtn.Add_Click({})
-
-$TxtBtn = New-Object System.Windows.Forms.Button
-	$TxtBtn.Location = New-Object System.Drawing.Point(100,300)
-	$TxtBtn.Size = New-Object System.Drawing.Size(100,30)
-	$TxtBtn.Text = "All Check"
-$Form.Controls.Add($TxtBtn)
+$AllCheck = New-Object System.Windows.Forms.Button
+	$AllCheck.Location = New-Object System.Drawing.Point(100,300)
+	$AllCheck.Size = New-Object System.Drawing.Size(100,30)
+	$AllCheck.Text = "AllCheck"
+$Form.Controls.Add($AllCheck)
 $TxtBtn.Add_Click({})
 
-$ClearBtn = New-Object System.Windows.Forms.Button
-	$ClearBtn.Location = New-Object System.Drawing.Point(200,300)
-	$ClearBtn.Size = New-Object System.Drawing.Size(100,30)
-	$ClearBtn.Text = "All Del"
-$Form.Controls.Add($ClearBtn)
-$ClearBtn.Add_Click({})
+$AllDel = New-Object System.Windows.Forms.Button
+	$AllDel.Location = New-Object System.Drawing.Point(200,300)
+	$AllDel.Size = New-Object System.Drawing.Size(100,30)
+	$AllDel.Text = "All Del"
+$Form.Controls.Add($AllDel)
+$AllDel.Add_Click({})
 
 $Form.Add_Shown({$Form.Activate()})
 $Form.ShowDialog()
